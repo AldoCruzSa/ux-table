70c12b6f454cad42f9ca0691898c2fa31498c77e
CRUZ SANDOVAL <uie94601@contiwan.com>
2023-02-15 23:35:19 -0600

    Updates

--- src/components/About.jsx
+++
@@ -1,7 +0,0 @@
-
-export const About = () => (
-    <div>
-        <h1>About</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, alias beatae tempore sapiente optio vitae. Amet ex porro accusantium, est eos, sapiente totam vitae repellat perspiciatis neque illum veniam dolor.</p>
-    </div>
-)
